28827d909e71e04a63d46b211f3a3f60837b7276
Matthew Scharley <matt.scharley@gmail.com>
2012-06-17 09:52:33 +1000

    Fleshed out Skype::Communication::Protocol

--- lib/skype/communication/protocol.rb
+++ lib/skype/communication/protocol.rb
@@ -1,9 +1,31 @@
 
+require 'observer'
+
 class Skype
   module Communication
     # Interface for the Skype::Communication::* classes. This provides basic input and output functionality with Skype.
+    #
+    # Includes Observable. Notifications sent will have a single string parameter which is the incoming command from
+    # Skype.
     module Protocol
+      include Observable
 
+      # Sends a message to Skype.
+      #
+      # Must be implemented by Protocol implementers.
+      def send(message)
+        raise "send(message) must be implemented."
+      end
+
+      private
+
+      # Internal method to implement Observable.
+      #
+      # Protocol implementers may use this to send notifications of incoming commands.
+      def receive(message)
+        changed
+        notify_observers(message)
+      end
     end
   end
 end
