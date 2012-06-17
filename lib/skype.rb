ee73d837501a97df05187d55bbc0d53e6ec1fa6c
Matthew Scharley <matt.scharley@gmail.com>
2012-06-17 10:33:51 +1000

    Add basic setup for DBus

--- lib/skype.rb
+++ lib/skype.rb
@@ -2,10 +2,14 @@
 # This class is the main interface between Ruby and Skype.
 
 class Skype
-  def initialize(communication_protocol = nil)
+  # Initialises the Skype library and sets up a communication protocol, but doesn't connect yet.
+  #
+  # application_name is for DBus's sake. It seems that on Windows/OSX Skype can get an application name for display to
+  # the user, but on DBus it expects one to be fed to it.
+  def initialize(application_name, communication_protocol = nil)
     if communication_protocol.nil?
       require 'skype/communication/dbus'
-      @skype = Skype::Communication::DBus.new
+      @skype = Skype::Communication::DBus.new(application_name)
     else
       @skype = communication_protocol
     end
--- lib/skype/communication/dbus.rb
+++ lib/skype/communication/dbus.rb
@@ -6,9 +6,27 @@ class Skype
   module Communication
     # This class handles communication with Skype via DBus.
     #
-    # This communication method is available under linux.
+    # This communication method is available under Linux.
     class DBus
       include Skype::Communication::Protocol
+
+      SKYPE_DBUS_SERVICE = 'com.Skype.API'
+      SKYPE_SERVER_PATH = '/com/Skype'
+      SKYPE_SERVER_INTERFACE = 'com.Skype.API'
+      SKYPE_CLIENT_PATH = '/com/Skype/Client'
+
+      def initialize(application_name)
+        @application_name = application_name
+        @dbus = ::DBus::SessionBus.instance
+        @dbus_service = @dbus.service(SKYPE_DBUS_SERVICE)
+        @skype = @dbus_service.object(SKYPE_SERVER_PATH)
+        @skype.introspect
+        @skype.default_iface = SKYPE_SERVER_INTERFACE
+      end
+
+      def send(message)
+        @skype.Invoke(message)
+      end
     end
   end
 end
